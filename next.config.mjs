e51eae2b2b6b2ccb400274cf7175e8dabf48b378
Santiago Aguilera <aguilerasanti@hotmail.com>
2025-04-23 01:34:52 -0300

    add mjs configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,6 +10,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: "export",
+  basePath: "/saskiaroemmelt",
 }
 
 export default nextConfig
